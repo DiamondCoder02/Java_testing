04c1cad1863d61a3013a1ced62d028c38ccc3f17
DiamondPRO02 <kovacsdanielmark@gmail.com>
2020-11-17 19:52:25 +0100

    first class

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,5 +1,9 @@
+import java.util.Arrays;
+
 public class Application {
     public static void main(String[] args) {
+        int[] szamtomb = new int[]{0, 012, 013452};
 
+        System.out.println(Arrays.toString(szamtomb));
     }
 }
