3333649b6fcc52f85000cc04b0b8b5e75a6cb1ed
DiamondPRO02 <kovacsdanielmark@gmail.com>
2020-11-18 18:19:04 +0100

    Calculator

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -47,18 +47,53 @@ public class Application {
             System.out.println("Komolyan? STUPID");
         }
     //True or false
-        if(igaz() && hamis()){
+        System.out.println("Test: && ");
+        if(hamis() && igaz()){
             System.out.println("Nem szabadna kiíródnia");
         }
-        if(igaz() & hamis()){
+        System.out.println("Test: & ");
+        if(hamis() & igaz()){
             System.out.println("Nem szabadna kiíródnia");
         }
-        if(igaz() || hamis()){
-            System.out.println("Nem szabadna kiíródnia");
+        System.out.println("Test: || ");
+        if(hamis() || igaz()){
+            System.out.println("Valamelyik Igaz");
         }
-        if(igaz() | hamis()) {
-            System.out.println("Nem szabadna kiíródnia");
+        System.out.println("Test: | ");
+        if(hamis() | igaz()) {
+            System.out.println("Valamelyik Igaz");
         }
+
+        System.out.println();
+        System.out.println();
+        System.out.println();
+
+        Double eredmeny2 = null;
+        switch (operacio){
+            case "+":
+                eredmeny2 = (double) szam1 + szam2;
+                break;
+            case "-":
+                eredmeny2 = (double) szam1 - szam2;
+                break;
+            default:
+                System.out.println("\nIsmeretlen operandus" + operacio);
+        }
+        System.out.println("Ez az 2. eredmény:" + eredmeny2);
+
+        Double eredmeny3 = null;
+        switch (operacio){
+            case "+":
+                eredmeny3 = (double) szam1 + szam2;
+                break;
+            case "-":
+                eredmeny3 = (double) szam1 - szam2;
+                break;
+            default:
+                System.out.println("\nIsmeretlen operandus" + operacio);
+        }
+        System.out.println("Hibás az eredmény:" + eredmeny3);
+
     }
     private static  boolean igaz(){
         System.out.println("igaz");
