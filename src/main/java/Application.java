2e3f8e3d78b90f019e959980bd86ad1d3e45641b
DiamondPRO02 <kovacsdanielmark@gmail.com>
2020-11-22 15:21:42 +0100

    Calculator

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,5 +1,28 @@
+
+import java.util.Scanner;
+
 public class Application {
-    public static void main(String[] args) {
+
+    public static void main(String[]args) {
+        System.out.println("1. Feladat");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Kérjük Írja be a mondatot!");
+        String mondat = scanner.nextLine();
+
+        String szoveg = mondat;
+        String[] darabok = szoveg.split(" ");
+
+        String lw = "";
+        for (int i = 0 ; i < darabok.length; i++ ){
+            // System.out.println(darabok[i]);
+            int mennyiseg = darabok[i].length();
+            int lfw = lw.length();
+            if (mennyiseg >= lfw) {
+                lw = darabok[i];
+            }
+        }
+        System.out.println(lw);
 
     }
 }
