e55ee44b702aecfe9a83563852e3780b5ed2f243
DiamondPRO02 <kovacsdanielmark@gmail.com>
2020-12-09 19:20:18 +0100

    HTML requests

---
+++ src/main/java/com/github/DiamondPRO02/request/UsernameRequest.java
@@ -0,0 +1,10 @@
+package com.github.DiamondPRO02.request;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Data
+public class UsernameRequest {
+    private String username;
+}
